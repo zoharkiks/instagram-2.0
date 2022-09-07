784af9d26a75ef38cfabe854cb66d289cdeffeb4
Zohar <zoharkiks@gmail.com>
2022-09-07 22:46:43 +0530

    Setup firebase and add custom signin page

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBI_8VW85BgLO_XVsJ9gcoIf4KAD0MRc_s",
+  authDomain: "instagram-2-57a85.firebaseapp.com",
+  projectId: "instagram-2-57a85",
+  storageBucket: "instagram-2-57a85.appspot.com",
+  messagingSenderId: "935229987365",
+  appId: "1:935229987365:web:10a2dc3da5b78e9f5f3dcc",
+  measurementId: "G-6TSPLC9PC4"
+};
+
+// Initialize Firebase
+const app = !getApps().length? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore()
+const storage = getStorage()
+
+export {app,db,storage}
+
---
+++ pages/auth/signin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <div>signin</div>
+  )
+}
+
+export default signIn
